08470b37c936419091376396d72d2f6f2def4bb4
Charleen Fei <fei.charleen@gmail.com>
2016-12-10 20:15:56 +0100

    started socket.io config on server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,21 @@ var cors = require('cors');
 // create new app and socket event listener
 var app = express();
 var http = require('http').Server(app);
+// hook socket onto http instantiation of server
+// set up socket authentication
 var io = require ('socket.io').listen(http);
+var socketioJwt = require('socketio-jwt');
+
+
+  io.on('connection', socketioJwt.authorize({
+      //need to configure client secret first
+      secret: 'YOUR_CLIENT_SECRET',
+      timeout: 1000 // 1 second to send the authentication message
+  })).on('authenticated', function(socket) {
+      //this socket is authenticated, we are good to handle more events from it.
+      console.log('hello! ' + JSON.stringify(socket.decoded_token));
+  });
+
 
 // listen on 2727...
 app.set('port', 2727);
