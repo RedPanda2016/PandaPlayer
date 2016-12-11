d6468d13a70770090ada001de5fc66aaaaa0d347
Duncan Nevin <duncan.nevin@gmail.com>
2016-12-10 19:53:31 -0800

    Authorization with bCrypt hasshing implemented

--- server/utilities/query.js
+++ server/utilities/query.js
@@ -1,17 +1,16 @@
 var db = require('../db/db_controller.js');
 
 module.exports = {
-  getAll: function(res) { // Gets all the users data and returns and array of objects.
-    db.Users.findAll().then(function(all){
-      if (res) { // if res is defined use response to create a json response. This is mostly for the API
-        res.json(all);
-        res.end();
-      } else { // else just return the data as is.
-        return all;
-      }
+  getAll: function(cb) { // Gets all the users data and returns and array of objects.
+    db.Users.findAll()
+    .then(function(data) {
+      cb(data);
+    })
+    .catch(function() {
+      console.log('failed GET request');
     });
   },
-  postReg: function(req) { // takes the JSON object for posting new data(see below)
+  postReg: function(req, cb) { // takes the JSON object for posting new data(see below)
     db.Users.findOrCreate({where: {
       userName: req.userName,
       firstName: req.firstName,
@@ -19,25 +18,25 @@ module.exports = {
       email: req.email,
       password: req.password
     },
+    })
+    .then(function() {
+      cb(201);
+    })
+    .catch(function() {
+      console.log('failed POST request');
+      cb(404);
     });
   },
-  getFiltered: function(req, res) { // takes an object with username --> {userName: 'DaName'}
-    db.Users.findAll({where: req})
+  getFiltered: function(params, cb) { // takes an object with username --> {userName: 'DaName'}
+    db.Users.findAll({where: params})
       .then(function(userPost) {
-        if (userPost) {
-          if (res) {
-            res.json(userPost);
-            res.end();
-          } else {
-            return userPost;
-          }
-          // may need to use a callback here?
-        } else {
-          return 404;
-        }
+        cb(userPost);
+      })
+      .catch(function() {
+        console.log('failed GET request');
       });
   },
-  putReg: function(req) { //alters user data based on userName/ takes an object with all data (see below) minus password
+  putReg: function(req, cb) { //alters user data based on userName/ takes an object with all data (see below) minus password
     db.Users.findAll({where: {userName: req.userName}})
       .then(function(userPost) {
         if (userPost) {
@@ -51,17 +50,25 @@ module.exports = {
               userName: req.userName
             }
           });
-        } else {
-          return 404;
         }
+        cb(201);
+      })
+      .catch(function() {
+        console.log('failed PUT request');
+        cb(404);
       });
   },
-  deleteReg: function(req) { // takes a JSON object (see below)
-    db.Users.findAll({where: req})
+  deleteReg: function(req, cb) { // takes a JSON object (see below)
+    db.Users.find({where: req})
       .then(function(userPost) {
         db.Users.destroy({
           where: req
         });
+        cb(201);
+      })
+      .catch(function() {
+        console.log('failed DELETE request');
+        cb(404);
       });
   }
 }
