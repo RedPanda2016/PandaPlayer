71bb3e59fbe61a1d64389430c896b5a07d16bb2b
Duncan Nevin <duncan.nevin@gmail.com>
2016-12-12 14:36:34 -0800

    Added sign in and sign up files

---
+++ app/components/sign-in/sign_in.jsx
@@ -0,0 +1,13 @@
+export default class signIn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+   render () {
+    return(
+      <div id="sign-up">
+        <text>TEST!</text>
+      </div>
+    )
+  }
+}
